255799d06504a587f547fa44753a58cc4a338adb
ZAF07 <zaffere07@gmail.com>
2021-08-17 15:50:09 +0800

    first commit?

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Effy removed the header</p>
+    </div>
+  );
+}
+
+export default App;
